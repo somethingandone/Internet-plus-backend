a202f9e47e308a01eb1348ff911247799f918b9a
yuhaofeng <2087953253@qq.com>
2023-11-27 14:26:05 +0800

    添加分割内规

---
+++ src/main/java/cn/edu/nju/model/InRegulationSplit.java
@@ -0,0 +1,23 @@
+package cn.edu.nju.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InRegulationSplit {
+    @Id
+    @Field("_id")
+    private String id;
+
+    private String title;
+
+    private String itemContent;
+}
