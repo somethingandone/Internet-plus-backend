df4759266d939fccd03c51bdcdba9dd769093e9a
Koala-H <73047557+Koala-H@users.noreply.github.com>
2024-01-12 10:26:01 +0800

    feat：增加了外归的测试

--- src/main/java/wsPackage/Test.java
+++ src/main/java/wsPackage/Test.java
@@ -6,6 +6,7 @@ public class Test {
   public static void main(String[] argv){
     BankPolicyWebServiceImplService bankPolicyWebServiceImplService = new BankPolicyWebServiceImplService();
     BankPolicyService bankPolicyService = bankPolicyWebServiceImplService.getBankPolicyWebServiceImplPort();
+    //内归
     InRegulation inRegulation = bankPolicyService.inRegulationGetById("65642eacb7fe9e690a7849ad");
     System.out.println(inRegulation.title);
     List<InRegulation> list = bankPolicyService.inRegulationList();
@@ -14,5 +15,14 @@ public class Test {
     }
     int count = bankPolicyService.inRegulationCount(inRegulation);
     System.out.println(count);
+    //外归
+    ExRegulation exRegulation = bankPolicyService.exRegulationGetById("6561e924c1e12dc3a62df122");
+    System.out.println(exRegulation.title);
+    List<ExRegulation> exRegulationList = bankPolicyService.exRegulationList();
+    for (ExRegulation regulation : exRegulationList) {
+      System.out.println(regulation.title);
+    }
+    int exRegulationCount = bankPolicyService.exRegulationCount(exRegulation);
+    System.out.println(exRegulationCount);
   }
 }
