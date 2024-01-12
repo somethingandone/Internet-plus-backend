65638b76ee39d3362e60f63d1805d5213d3a4c20
somethingandone <95566363+somethingandone@users.noreply.github.com>
2024-01-12 11:11:22 +0800

    update

--- src/main/java/cn/edu/nju/Application.java
+++ src/main/java/cn/edu/nju/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @EnableGlobalMethodSecurity(prePostEnabled = false)
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-//@SpringBootApplication()
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication()
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
